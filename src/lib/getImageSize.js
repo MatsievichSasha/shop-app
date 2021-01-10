e81f6b6dc848921e3ccaa4e1e60376b6b59b801e
MatsievichSasha <matsievich.sasha@gmail.com>
2021-01-10 15:15:36 +0200

    check type image

--- src/lib/getImageSize.js
+++ src/lib/getImageSize.js
@@ -1,41 +1,51 @@
 import { ACTIONS } from "../components/cards/cardsContext/cardsReduser"
 
-export const imageSize = async (name, image, dispatch, formState) => {
-  let promise = new Promise((resolve, reject) => {
-    try {
-      const fileReader = new FileReader()
+export const imageSize = async (name, file, dispatch, formState) => {
 
-      fileReader.onload = () => {
-        const img = new Image()
+  let hasError = false
+  let error = ''
 
-        img.onload = () => {
-          resolve({ width: img.width, height: img.height })
+  if (file.type === "image/jpeg" || file.type === "image/pjpeg" || file.type === "image/png" || file.type === "image/jpg") {
+    let promise = new Promise((resolve, reject) => {
+      try {
+        const fileReader = new FileReader()
+
+        fileReader.onload = () => {
+          const img = new Image()
+
+          img.onload = () => {
+            resolve({ width: img.width, height: img.height })
+          }
+
+          img.src = fileReader.result
         }
 
-        img.src = fileReader.result
+        fileReader.readAsDataURL(file)
+      } catch (e) {
+        reject(e)
       }
+    });
 
-      fileReader.readAsDataURL(image)
-    } catch (e) {
-      reject(e)
+    let { width, height } = await promise;
+
+    const ImageMaxSize = 4000;
+    const ImageMinSize = 200;
+
+    console.log(0, file.type)
+
+    console.log(1, file.type)
+    if (width < ImageMinSize || height < ImageMinSize || width > ImageMaxSize || height > ImageMaxSize) {
+      hasError = true;
+      error = `Недопустимое разрешение(Ваше изображения ${width}px*${height}px). Допустимое разрешение min = 200px, max = 4000px`
+    } else {
+      error = ""
     }
-  });
-
-  let { width, height } = await promise;
-
-  const ImageMaxSize = 4000;
-  const ImageMinSize = 200;
-  let hasError = false
-  let error
-
-  if (width < ImageMinSize || height < ImageMinSize || width > ImageMaxSize || height > ImageMaxSize) {
-    hasError = true;
-    console.log('Error size')
-    error = `Недопустимое разрешение(Ваше изображения ${width}px*${height}px). Допустимое разрешение min = 200px, max = 4000px`
   } else {
-    error = ""
-    console.log('OK!')
+    hasError = true;
+    console.log(2, file.type)
+    error = "Недопустимый формат файла. Выберите другой файл (.jpg, .jpeg,.png)."
   }
+
   let isFormValid = true
   dispatch({
     type: ACTIONS.CHANGE_FIELD,
